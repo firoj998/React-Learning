446351c91f01e35ae981e78dfd37c142a65878ce
Mohd Firoj <firojqv@gmail.com>
2024-01-16 11:47:49 +0530

    Outputing List data dynamically

--- 01startingProject_UDEMY/src/App.jsx
+++ 01startingProject_UDEMY/src/App.jsx
@@ -31,7 +31,11 @@ function App() {
           description = "The Core UI Building Block!"
           image = {componentsImg}
           /> */}
-          <CoreConcept
+
+          {CORE_CONCEPTS.map((conceptItem)=>(<CoreConcept key={conceptItem.title} {...conceptItem} />)
+          )}
+          
+          {/* <CoreConcept
             title={CORE_CONCEPTS[0].title}
             description={CORE_CONCEPTS[0].description}
             image={CORE_CONCEPTS[0].image}
@@ -50,7 +54,8 @@ function App() {
             title={CORE_CONCEPTS[3].title}
             description={CORE_CONCEPTS[3].description}
             image={CORE_CONCEPTS[3].image}
-          />
+          /> */}
+
         </ul>
         </section>
       <section id = "examples">
