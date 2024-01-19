63a99620f3da3da5742ca25ca9313c9d3afc0c7e
Mohd Firoj <firojqv@gmail.com>
2024-01-20 00:13:23 +0530

    Life cycle Method and state in Class Component

--- BLOG/src/App.jsx
+++ BLOG/src/App.jsx
@@ -1,16 +1,16 @@
 // import { useState } from 'react'
 import './App.css'
 import ClassComponent from './ClassComponent'
+import Contact from './Contact'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello Doston!</h1>
+      <h1>Hello World!</h1>
       <ClassComponent />
-      <ClassComponent />
-      <ClassComponent />
+      {/* <Contact /> */}
     </>
   )
 }
--- BLOG/src/ClassComponent.jsx
+++ BLOG/src/ClassComponent.jsx
@@ -1,11 +1,51 @@
 import React from "react";
 class ClassComponent extends React.Component{
+constructor(){
+    super();
+    console.warn("constructor called");
+    this.state = {
+        name:"Firoj",
+        age:25
+    }
+}
+
+componentDidMount(){
+    console.warn("did mount")
+  
+}
+componentDidUpdate(){
+    alert("name updated!")
+}
+
     render(){
         return (
             <div>
-                Hello From ClassComponent..!
+               <h2> ClassComponent..!</h2>
+               <h3>{this.state.name}</h3>
+               <h3>{this.state.age}</h3>
+               <button onClick={()=>{this.setState({name:"Mohd Firoj"})}}>Update State</button>
             </div>
-        )
+        );
     }
 }
 export default ClassComponent;
+
+
+
+// Life Cycle Method-------
+
+// constructor(){
+
+// }
+
+// componentDidMount(){
+
+// }
+
+// componentDidUpdate(){
+
+// }
+
+// componentWillMount(){
+
+// }
