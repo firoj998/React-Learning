c081ee761369e6520e8ef4d6090fadb0a9745e94
Mohd Firoj <firojqv@gmail.com>
2024-01-28 23:41:40 +0530

    deriving state from props, sharing state accroos the components, disable button dynamically

--- 02-tic-tac-toe-project/src/App.jsx
+++ 02-tic-tac-toe-project/src/App.jsx
@@ -1,12 +1,33 @@
 import { useState } from "react";
 import GameBoard from "./Components/GameBoard";
 import Player from "./Components/Player";
-
+import Log from "./Components/Log";
+  
+function deriveActivePlayer(gameTurns){
+    let currentPlayer = 'X';
+    if( gameTurns.length>0 && gameTurns[0].player === 'X')
+    {
+      currentPlayer = 'O';
+    }
+    return currentPlayer;
+  }
 function App() {
-  const [activePlayer , setActivePlayer] = useState('X');
+  // const [activePlayer , setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState([]);
+  const activePlayer = deriveActivePlayer(gameTurns);
 
-  function handleSelectSquare(){
-    setActivePlayer((currActivePlayer) =>currActivePlayer ==='X'?'O':'X')
+  function handleSelectSquare(rowIndex,colIndex)
+  {
+    // setActivePlayer((currActivePlayer) => (currActivePlayer === 'X' ? 'O' : 'X'))
+    
+    setGameTurns((prevTurns)=>{
+     const currentPlayer = deriveActivePlayer(prevTurns)
+     
+      const updatedTurns = [{square:{row:rowIndex,col:colIndex} , player:currentPlayer}, ...prevTurns];
+      return updatedTurns;
+      
+    })
+   
   }
   
   return (
@@ -16,8 +37,12 @@ function App() {
           <Player initialName="Player1" symbol="X" isActive={activePlayer ==='X'}/>
           <Player initialName="Player2" symbol="O" isActive={activePlayer ==='O'}/>
         </ol>
-      <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+      <GameBoard onSelectSquare={handleSelectSquare}
+       turns={gameTurns} 
+       />
+     
       </div>
+      <Log  turns = {gameTurns}/>
     </main>
   )
 }
--- 02-tic-tac-toe-project/src/Components/GameBoard.jsx
+++ 02-tic-tac-toe-project/src/Components/GameBoard.jsx
@@ -1,21 +1,28 @@
-import { useState } from "react";
+
 
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
-export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({onSelectSquare, turns}) {
+  let  gameBoard = initialGameBoard;
+ for (const turn of turns) {
+      const {square, player} = turn;
+      const {row, col} = square;
+      gameBoard[row][col] = player;
 
-  function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard((prevGameBoard) => {
-      const updatedBoard = [...prevGameBoard.map((innerArray) => [...innerArray])];  // onjects or Arrays should be updated imutable way [https://academind.com/tutorials/reference-vs-primitive-values ]
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedBoard;
-    });
-    onSelectSquare();
-  }
+ }
+//   const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+//   function handleSelectSquare(rowIndex, colIndex) {
+//     setGameBoard((prevGameBoard) => {
+//       const updatedBoard = [...prevGameBoard.map((innerArray) => [...innerArray])];  // onjects or Arrays should be updated imutable way [https://academind.com/tutorials/reference-vs-primitive-values ]
+//       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+//       return updatedBoard;
+//     });
+//     onSelectSquare();
+//   }
 
   return (
     <ol id="game-board">
@@ -24,7 +31,7 @@ export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={()=>{handleSelectSquare(rowIndex,colIndex)}}>{playerSymbol}</button>
+                <button onClick={()=>onSelectSquare(rowIndex,colIndex)} disabled={playerSymbol != null}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
